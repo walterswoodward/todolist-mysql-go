4e28c495f555b0ac431643277693f48c8f2a5df7
walterswoodward <walter.sekope.woodward@gmail.com>
2021-02-25 18:29:52 -0800

    Adds POST /todo/{id} route and corresponding UpdateItem and GetItemByID funcs

--- todolist.go
+++ todolist.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
-	"github.com/gorilla/mux"
-	log "github.com/sirupsen/logrus"
+	"strconv"
+
 	_ "github.com/go-sql-driver/mysql" // To import a package solely for its side-effects (initialization), use the blank identifier
+	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"encoding/json"
+	log "github.com/sirupsen/logrus"
 )
 
 var db, _ = gorm.Open("mysql", "root:root@/todolist?charset=utf8&parseTime=True&loc=Local")
@@ -19,16 +21,26 @@ type TodoItemModel struct {
 	Completed bool
 }
 
+// Helpers
 func GetTodoItems(completed bool) interface{} {
 	var todos []TodoItemModel
 	TodoItems := db.Where("completed = ?", completed).Find(&todos).Value
 	return TodoItems
 }
 
+func GetItemByID(Id int) bool {
+	todo := &TodoItemModel{}
+	result := db.First(&todo, Id)
+	if result.Error != nil {
+		log.Warn("TodoItem not found in database")
+		return false
+	}
+	return true
+}
+
 // GET Functions
 func HealthCheck(w http.ResponseWriter, r *http.Request) {
 	log.Info("API Health is OK")
-	// set header + write to view
 	w.Header().Set("Content-Type", "application/json")
 	io.WriteString(w, `{"active": true}`)
 }
@@ -59,10 +71,35 @@ func CreateItem(w http.ResponseWriter, r *http.Request) {
 	db.Create(&todo)
 	// Use db.Last(...) to get the newly added row in todo_item_models
 	result := db.Last(&todo)
-	// Set response header
 	w.Header().Set("Content-Type", "application/json")
 	// Return JSON response
 	json.NewEncoder(w).Encode(result.Value)
+}
+
+func UpdateItem(w http.ResponseWriter, r *http.Request) {
+	// Get URL parameters from mux
+	vars := mux.Vars(r)
+	// Get value for "id", convert to integer, short assign to id
+	id, _ := strconv.Atoi(vars["id"])
+	// Test if the TodoItem exists in the DB
+	err := GetItemByID(id)
+
+	if err == false {
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, `{"updated": false, "error": "Record Not Found"}`)
+	} else {
+		// strconv.ParseBool returns 2 values, the second of which being an
+		// error if passed an invalid value so we use the blank identifier `_`,
+		// otherwise at compilation time you'll get a assignment mismatch error 
+		completed, _ := strconv.ParseBool(r.FormValue("completed"))
+		log.WithFields(log.Fields{"Id": id, "Completed": completed}).Info("Updating TodoItem")
+		todo := &TodoItemModel{}
+		db.First(&todo, id)
+		todo.Completed = completed
+		db.Save(&todo)
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, `{"updated": true}`)
+	}
 }
 
 
@@ -90,5 +127,6 @@ func main() {
 	router.HandleFunc("/complete", GetCompleteItems).Methods("GET")
 	// POST
 	router.HandleFunc("/todo", CreateItem).Methods("POST")
+	router.HandleFunc("/todo/{id}", UpdateItem).Methods("POST")
 	http.ListenAndServe(":8000", router)
 }
