db8d4b01a699a32078cef372c10b49a4a99ad36e
walterswoodward <walter.sekope.woodward@gmail.com>
2021-03-03 09:25:11 -0800

    Removes validator struct tag from Completed as any invalid input will be caught with strconv.ParseBool

--- todolist.go
+++ todolist.go
@@ -20,7 +20,7 @@ var db, _ = gorm.Open("mysql", "root:root@/todolist?charset=utf8&parseTime=True&
 type TodoItemModel struct {
 	Id int `gorm:"primary_key"`
 	Description string `validate:"min=1,max=40,regexp=^[a-zA-Z]*$"`
-	Completed bool `validate:"regexp=^true$|^false$"`
+	Completed bool
 }
 
 // Helpers
